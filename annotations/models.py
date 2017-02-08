63817c423f07e34e9c31b581c2847d4b85da054a
Tyler Fisher <tfisher@npr.org>
2017-02-08 12:41:30 -0500

    allow author image/author page to be null

--- annotations/models.py
+++ annotations/models.py
@@ -26,8 +26,8 @@ class Author(models.Model):
     first_name = models.CharField(max_length=50)    
     last_name = models.CharField(max_length=50)
     author_title = models.CharField(max_length=100)
-    author_image = models.URLField()
-    author_page = models.URLField()
+    author_image = models.URLField(null=True, blank=True)
+    author_page = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
