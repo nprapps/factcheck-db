4426b16c037720f64f78d5cedc687879485775ea
Tyler Fisher <tyler@tylerjfisher.com>
2017-03-06 15:00:26 -0500

    determine the layout of the tweet

--- annotations/signals.py
+++ annotations/signals.py
@@ -1,11 +1,15 @@
 import app_config
 import json
 import os
+import requests
 import subprocess
 
+from bs4 import BeautifulSoup
 from django.db.models.signals import post_save, m2m_changed, post_delete
 from django.dispatch import receiver
 from .models import Annotation
+
+TWITTER_OEMBED_URL = 'https://api.twitter.com/1.1/statuses/oembed.json'
 
 @receiver(post_save)
 @receiver(m2m_changed, sender=Annotation.claims.through)
@@ -29,7 +33,8 @@ def publish_json(sender, instance, **kwargs):
                     'type': claim.claim_type,
                     'id': claim.twitter_id(),
                     'date': claim.claim_date.isoformat(),
-                    'media': claim.show_media
+                    'media': claim.show_media,
+                    'layout': get_claim_layout(claim)
                 }
 
                 claims.append(claim_data)
@@ -60,3 +65,21 @@ def sort_annotations(block):
         return block['claims'][-1]['date']
     else:
         return '0'
+
+def get_claim_layout(claim):
+    layout = 'text'
+
+    if claim.show_media:
+        id = claim.twitter_id()
+        response = requests.get(TWITTER_OEMBED_URL, params=(('id', id),))
+        response.raise_for_status()
+        data = response.json()
+        soup = BeautifulSoup(data['html'])
+        links = soup.find_all('a')
+        for link in links:
+            if link.text == link.attrs['href']:
+                layout = 'attached_link'
+            if 'pic.twitter.com' in link.text:
+                layout = 'image'
+
+    return layout
